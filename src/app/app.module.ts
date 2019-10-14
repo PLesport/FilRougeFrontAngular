6d0b5721b2ee4ffa76c23b35d36c2e2144be8513
PLesport <pierre.lesport@gmail.com>
2019-10-14 09:42:51 +0200

    !

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,59 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ErrorPage404Component } from './error-page404/error-page404.component';
+import { CartComponent } from './cart/cart.component';
+import { CategoryPageComponent } from './category-page/category-page.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { ContactComponent } from './contact/contact.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { MyProfileComponent } from './my-profile/my-profile.component';
+import { RegisterComponent } from './register/register.component';
+import { SearchComponent } from './search/search.component';
+import { AddProductComponent } from './add-product/add-product.component';
+import { CategoryComponent } from './category/category.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { OrderComponent } from './order/order.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { StatsComponent } from './stats/stats.component';
+import { TransactionComponent } from './transaction/transaction.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { ProfileDetailComponent } from './profile-detail/profile-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorPage404Component,
+    CartComponent,
+    CategoryPageComponent,
+    CheckoutComponent,
+    ContactComponent,
+    DashboardComponent,
+    LoginComponent,
+    ProductPageComponent,
+    MyProfileComponent,
+    RegisterComponent,
+    SearchComponent,
+    AddProductComponent,
+    CategoryComponent,
+    HomepageComponent,
+    AdminLoginComponent,
+    OrderComponent,
+    ProductListComponent,
+    StatsComponent,
+    TransactionComponent,
+    UserListComponent,
+    ProfileDetailComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [ErrorPage404Component, CartComponent, CategoryPageComponent, CheckoutComponent, ContactComponent, DashboardComponent, LoginComponent, ProductPageComponent, MyProfileComponent, RegisterComponent, SearchComponent, AddProductComponent, CategoryComponent, HomepageComponent, AdminLoginComponent, OrderComponent, ProductListComponent, StatsComponent, TransactionComponent, UserListComponent, ProfileDetailComponent]
 })
 export class AppModule { }
