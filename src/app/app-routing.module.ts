c356ec3a669dc6ab219e4ee55944fe8c4a1ca419
PLesport <pierre.lesport@gmail.com>
2019-10-14 17:39:11 +0200

    save before changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProductPageComponent } from './product-page/product-page.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { LoginComponent } from './login/login.component';
 
 
 const routes: Routes = [
+  { path: 'product-page', component: ProductPageComponent },
+  { path: 'login', component: LoginComponent },
+
 ];
 
 @NgModule({
