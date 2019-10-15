d84586e032f81345eea9ffc59f447f47805d7fee
PLesport <pierre.lesport@gmail.com>
2019-10-15 10:47:34 +0200

    Modif Homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { LoginComponent } from './login/login.component';
+import { CategoryPageComponent } from './category-page/category-page.component';
+import { CartComponent } from './cart/cart.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 
 const routes: Routes = [
+  { path: 'category-page' , component: CategoryPageComponent},
   { path: 'product-page', component: ProductPageComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'cart' , component: CartComponent},
+  { path: '' , component: HomepageComponent},
 
 ];
 
--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./homepage.component.scss']
 })
 export class HomepageComponent implements OnInit {
-
+ images = [
+    `/assets/images/home-banner/the-ou-cafe.jpg`,
+    `https://picsum.photos/900/500?random&t=${Math.random()}`,
+    `https://picsum.photos/900/500?random&t=${Math.random()}`
+  ];
   constructor() { }
 
   ngOnInit() {
   }
+
 
 }
