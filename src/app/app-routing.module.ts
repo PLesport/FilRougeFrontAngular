64b657ce24336ec321dbcc9882c9fd2437e70529
plesport <pierre.lesport@gmail.com>
2019-10-16 20:49:09 +0200

    Commit du soir, remise en état liens User

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ProductPageComponent } from './product-page/product-page.component';
-import { LoginComponent } from './login/login.component';
-import { CategoryPageComponent } from './category-page/category-page.component';
-import { CartComponent } from './cart/cart.component';
-import { HomepageComponent } from './homepage/homepage.component';
-import { AddProductComponent } from './add-product/add-product.component';
-import { AdminIndexComponent } from './admin-index/admin-index.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { UserListComponent } from './user-list/user-list.component';
-import { OrderComponent } from './order/order.component';
+
 import { AdminUserProfileComponent } from './admin-user-profile/admin-user-profile.component';
 import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { AddProductComponent } from './add-product/add-product.component';
+import { AdminIndexComponent } from './admin-index/admin-index.component';
+import { BlogComponent } from './blog/blog.component';
+import { CategoryPageComponent } from './category-page/category-page.component';
+import { CartComponent } from './cart/cart.component';
 import { CheckoutComponent } from './checkout/checkout.component';
 import { ContactComponent } from './contact/contact.component';
-import {UserDashboardComponent } from './user-dashboard/user-dashboard.component';
-import { SearchComponent } from './search/search.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { LoginComponent } from './login/login.component';
+import { OrderComponent } from './order/order.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductPageComponent } from './product-page/product-page.component';
 import { RegisterComponent } from './register/register.component';
+import { SearchComponent } from './search/search.component';
 import { TransactionComponent } from './transaction/transaction.component';
 import { UserHistoriqueComponent } from './user-historique/user-historique.component';
 import { UserModifierComponent } from './user-modifier/user-modifier.component';
 import { UserSearchComponent } from './user-search/user-search.component';
-import { BlogComponent } from './blog/blog.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
 
 const routes: Routes = [
 
@@ -41,7 +42,7 @@ const routes: Routes = [
   { path: 'register' , component: RegisterComponent},
   { path: 'search' , component: SearchComponent},
   { path: 'transation' , component: TransactionComponent},
-  { path: 'dashboard' , component: UserDashboardComponent},
+  { path: 'user-dashboard' , component: UserDashboardComponent},
   { path: 'user-list', component: UserListComponent},
   { path: 'user-historique' , component: UserHistoriqueComponent},
   { path: 'user-modifier', component: UserModifierComponent},
