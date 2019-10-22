47076901f6a3b4b6aaf2c5ba126a14f6e8631972
PLesport <pierre.lesport@gmail.com>
2019-10-22 17:05:47 +0200

    Mardi Aprem

--- src/app/category-page/category-page.component.ts
+++ src/app/category-page/category-page.component.ts
@@ -15,6 +15,7 @@ export class CategoryPageComponent implements OnInit {
     constructor(private categoryService: CategoryService, private router: Router) { }
 
   inputCtrl: FormControl;
+  boxCtrl: FormControl;
   availableValues: Product[];
   displayValues: Product[];
   subs: Subscription;
@@ -23,21 +24,31 @@ export class CategoryPageComponent implements OnInit {
   ngOnInit() {
     this.subs = new Subscription();
     this.inputCtrl = new FormControl('');
+    this.boxCtrl = new FormControl('');
     this.categoryService.getProducts().subscribe(products => {
       this.availableValues = products;
       this.displayValues = products;
     });
     this.subs.add(
-      this.inputCtrl.valueChanges.subscribe(value => {
+    this.inputCtrl.valueChanges.subscribe(value => {
         this.displayValues = this.filterValues(value, this.availableValues, 'description');
         // "name" : le nom de l'attribut de l'objet Product qui doit matcher
-      })
+      }).add(
+    this.boxCtrl.valueChanges.subscribe(value => {
+        this.displayValues = this.filterValuesByType(value, this.availableValues, 'type')
+        })
+      )
     );
   }
 
   filterValues(value: string, availableValues: Product[], attribute): Product[] {
     return availableValues.filter(availableValue => availableValue[attribute].indexOf(value) !== -1);
   }
+
+  filterValuesByType(value: string, availableValues: Product[], attribute): Product[] {
+    return availableValues.filter(availableValue => availableValue[attribute].indexOf(value) !== -1);
+  }
+
 
   onSubmit() {
     this.categoryService.postProduct(this.productModel)
